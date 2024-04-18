835e6ed003e020eac89f67ca34c3460e73cc2647
deioh <gamekuma@gmail.com>
2024-04-18 16:02:04 +0800

    Update task-01.js
    Number of Categories

---
+++ js/task-01.js
@@ -0,0 +1,33 @@
+let totalCategories = document.getElementsByClassName("item");
+console.log("Number of Categories = " + totalCategories.length);
+
+// let uList = document.getElementById("categories");
+// let allList = document.querySelectorAll(".item");
+// checkBtn.onclick = () => {
+//   alert("hello");
+//   let li = document.createElement("li");
+//   li.classList = "item";
+
+//   //li.setAttribute("target", "_blank");
+//   // li.textContent = "Click me!";
+//   //li.innerText = <a href = "#" >
+
+//   uList.appendChild(li);
+//   console.log(uList);
+// };
+
+//let item2 = (document.getElementById("categories").style.color = "red");
+
+//let headings = document.getElementsByClassName("heading"); //.style.color = "red");
+
+// let ul = document.querySelectorAll(".categories");
+
+// headings.forEach((element) => {
+//   element.style.color = "blue";
+// });
+
+// for (let index = 0; index < item.length; index++) {
+//   item[index].style.color = "red";
+// }
+
+//console.log(checkBtn);

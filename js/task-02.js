b384a5d8fd11e9588bc82e5d7bb048781579fd1b
deioh <143625578+deioh@users.noreply.github.com>
2024-04-19 16:15:24 +0800

    Update task-02.js
    task-02.js fin

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ul = document.getElementById('ingredients')
+
+for (const item of ingredients) {
+  const li = document.createElement("li");
+  li.textContent = item;
+  ul.appendChild(li);
+}
+

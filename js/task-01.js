da18e49fa39a9345226175ce3badd234fdbd2a25
deioh <143625578+deioh@users.noreply.github.com>
2024-04-19 15:12:51 +0800

    Update task-01.js
    task-01.js fin

---
+++ js/task-01.js
@@ -0,0 +1,23 @@
+//const message = 'Hello world' // Try edit me
+// Update header text
+//document.querySelector('#header').innerHTML = message
+//console.log(categoriesQuery[0].firstElementChild.innerHTML)
+//console.log(categoriesQuery[0].firstElementChild.nextElementSibling.childElementCount)
+//console.log(categoriesQuery)
+
+
+// number of categories
+let categoriesItem = document.getElementsByClassName("item")
+let categoriesQuery = document.querySelectorAll(".item")
+
+// Log to console
+console.log(`Number of categories: ${categoriesItem.length}`)
+
+for (let i = 0; i < categoriesQuery.length; i++) {    
+    console.log(`Categories : ${categoriesQuery[i].firstElementChild.innerHTML}`)
+    console.log(`Elements: ${categoriesQuery[i].firstElementChild.nextElementSibling.childElementCount}`)            
+    };
+
+
+
+

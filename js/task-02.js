720d239df1d5c98e643bd112cf641929d21baf98
deioh <143625578+deioh@users.noreply.github.com>
2024-04-19 16:26:29 +0800

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,9 +7,10 @@ const ingredients = [
   'Condiments',
 ];
 
-
+// get ul ID
 const ul = document.getElementById('ingredients')
 
+// loop for li insert
 for (const ingredient of ingredients) {
   const li = document.createElement("li");
   li.classList.add("item")

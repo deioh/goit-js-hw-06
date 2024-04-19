5daba6058c249dcd1330ef9f366c1dd96262da95
deioh <143625578+deioh@users.noreply.github.com>
2024-04-19 16:23:34 +0800

    Update task-02.js
    added classname

--- js/task-02.js
+++ js/task-02.js
@@ -12,6 +12,7 @@ const ul = document.getElementById('ingredients')
 
 for (const ingredient of ingredients) {
   const li = document.createElement("li");
+  li.classList.add("item")
   li.textContent = ingredient;
   ul.appendChild(li);
 }

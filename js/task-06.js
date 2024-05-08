317804a1bbd86a688dab51d88be1b92af530facc
deioh <143625578+deioh@users.noreply.github.com>
2024-05-09 02:09:46 +0800

    updated styles

---
+++ js/task-06.js
@@ -0,0 +1,19 @@
+const inputElement = document.querySelector("#validation-input");
+
+inputElement.onblur = () => {
+  const enteredLength = inputElement.value.length;
+  //const requiredLength = document.querySelector("#data-length");
+  const requiredLength = Number(inputElement.getAttribute("data-length"));
+
+  if (enteredLength === requiredLength) {
+    console.log("Correct length");
+
+    inputElement.classList.remove("invalid"); // Remove 'invalid' class
+    inputElement.classList.add("valid"); // Add 'valid' class
+  } else {
+    console.log("Incorrect length");
+
+    inputElement.classList.remove("valid"); // Remove 'valid' class
+    inputElement.classList.add("invalid"); // Add 'invalid' class
+  }
+};

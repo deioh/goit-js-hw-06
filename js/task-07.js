62502ccd74338fa81afe78eb283087dd90f3de41
deioh <143625578+deioh@users.noreply.github.com>
2024-05-09 21:50:44 +0800

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,21 @@
-const fontSizeControl = document.querySelector("#font-size-control");
-const text = document.querySelector("#text");
+/**
+ * Updates text font size based on the value of the range input
+ */
+const fontSizeControl = document.querySelector("input#font-size-control");
+
+/**
+ * Target element to change font size
+ */
+const text = document.querySelector("span#text");
 
 fontSizeControl.oninput = () => {
+  /**
+   * New font size value
+   */
   const fontSize = fontSizeControl.value;
+
+  /**
+   * Apply new font size to the target element
+   */
   text.style.fontSize = `${fontSize}px`;
 };

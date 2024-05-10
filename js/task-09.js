41cc6440fa6ceeb661c05ec8239b64b1cbd6b061
deioh <gamekuma@gmail.com>
2024-05-10 15:03:29 +0800

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,27 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+/**
+ * Change color button
+ */
+const changeColorBtn = document.querySelector(".change-color");
+
+/**
+ * Element which displays current color
+ */
+const colorElement = document.querySelector(".color");
+
+/**
+ * Event listener for the button
+ */
+changeColorBtn.onclick = () => {
+  // Generate random color
+  const color = getRandomHexColor();
+
+  // Update color element
+  colorElement.textContent = color;
+
+  // Change page background color
+  document.body.style.backgroundColor = color;
+};
